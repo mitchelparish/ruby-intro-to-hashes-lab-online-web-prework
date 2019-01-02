42da736db43da40fe7242eaed64510b6eb10b62c
Mitchel Parish <mitchelparish@gmail.com>
2019-01-02 19:21:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,14 +18,14 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads => {pieces: 4}
+		:railroads => {:pieces => 4}
 }
 
 end
 
 def monopoly_with_third_tier
 	monopoly = {
-		:railroads => {pieces: 4, "names" => {}}
+		:railroads => {:pieces => 4, :names =>
 }
 
 end
